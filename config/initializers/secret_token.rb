e6117a7c09e81729996746e51fa61f4db356a76b
juve <juve.guz@gmail.com>
2012-02-21 23:54:49 -0600

    primer comir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fe32fbf8744a943b07ed8b788f84afe8be3f4b076bef340fd0a9400f1aa8053dcf90496bfbc8b5759e42cb051e9274b48bc513fc5df99f4d18ac35dbb39b7a02'
